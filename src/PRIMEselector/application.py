de83c9b1363c84d486b7a4d1dd319259bb8612b9
Jens Sauer <jsauer65@gmail.com>
2015-08-31 22:31:38 +0200

    Installed a Build System and moved some files to the correct destination

--- src/PRIMEselector/application.py
+++ src/PRIMEselector/application.py
@@ -2,25 +2,32 @@
 from gi.repository import Gtk, GObject
 import subprocess
 
-class PrimeSelector(Gtk.Window):
+class PRIMEselector(Gtk.Window):
 
-    def __init__(self):
-	Gtk.Window.__init__(self, title="PRIME Selector")
+    datadir = ""
+    def __init__(self, *args, **kwargs):
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+	self._create_main_window()
+
+    def _create_main_window(self):
+	self.window = Gtk.Window()
 	grid = Gtk.Grid()
 	grid.set_property("margin", 18)
 	grid.set_row_spacing(6)
 	grid.set_column_spacing(12)
 	grid.set_column_homogeneous(True)
-	self.add(grid)
+	self.window.add(grid)
 
 	label = Gtk.Label()
 	label.set_markup("<b>Select the GPU you want to use</b>")
 	grid.attach(label, 0, 0, 2, 1)
 
+	print("pkgdatadir: %s" % self.pkgdatadir)
 	self.intel_button = Gtk.Button()
 	self.nvidia_button = Gtk.Button()
-	nvidia_image = Gtk.Image.new_from_file("images/nvidia-logo.png")
-	intel_image = Gtk.Image.new_from_file("images/intel-logo.png")
+	nvidia_image = Gtk.Image.new_from_file("%s/nvidia-logo.png" % self.pkgdatadir)
+	intel_image = Gtk.Image.new_from_file("%s/intel-logo.png" % self.pkgdatadir)
 	self.intel_button.set_image(intel_image)
 	self.nvidia_button.set_image(nvidia_image)
 	grid.attach(self.nvidia_button, 0, 1, 1, 1)
@@ -40,8 +47,13 @@ class PrimeSelector(Gtk.Window):
 	else:
 	    self.set_sensitive_button(self.intel_button, self.nvidia_button)
 
+	self.window.connect("delete-event", Gtk.main_quit)
 	self.intel_button.connect("clicked", self.on_intel_clicked)
 	self.nvidia_button.connect("clicked", self.on_nvidia_clicked)
+
+    def run(self):
+	self.window.show_all()
+	Gtk.main()
 
     def get_prime_status(self):
 	if "intel" in subprocess.check_output(["prime-select", "query"]):
@@ -58,8 +70,3 @@ class PrimeSelector(Gtk.Window):
 
     def on_nvidia_clicked(self, button):
 	self.set_sensitive_button(self.intel_button, self.nvidia_button)
-
-win = PrimeSelector()
-win.connect("delete-event", Gtk.main_quit)
-win.show_all()
-Gtk.main()
