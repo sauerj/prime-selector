289731ab0f44ba39946ef9a07dd5da9383442ec6
Jens Sauer <jsauer65@gmail.com>
2015-08-31 02:03:20 +0200

    added logos for intel and nvidia first UI setup 	* correct detection of current prime status buttons are clickable, but no changes are made

---
+++ prime-selector.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+from gi.repository import Gtk, GObject
+import subprocess
+
+class PrimeSelector(Gtk.Window):
+
+    def __init__(self):
+	Gtk.Window.__init__(self, title="PRIME Selector")
+	grid = Gtk.Grid()
+	grid.set_property("margin", 18)
+	grid.set_row_spacing(6)
+	grid.set_column_spacing(12)
+	grid.set_column_homogeneous(True)
+	self.add(grid)
+
+	label = Gtk.Label()
+	label.set_markup("<b>Select the GPU you want to use</b>")
+	grid.attach(label, 0, 0, 2, 1)
+
+	self.intel_button = Gtk.Button()
+	self.nvidia_button = Gtk.Button()
+	nvidia_image = Gtk.Image.new_from_file("images/nvidia-logo.png")
+	intel_image = Gtk.Image.new_from_file("images/intel-logo.png")
+	self.intel_button.set_image(intel_image)
+	self.nvidia_button.set_image(nvidia_image)
+	grid.attach(self.nvidia_button, 0, 1, 1, 1)
+	grid.attach(self.intel_button, 1, 1, 1, 1)
+
+	label2 = Gtk.Label("Logout")
+	label2.set_halign(2)
+	grid.attach(label2, 0, 2, 1, 1)
+
+	logout_switch = Gtk.Switch()
+	logout_switch.set_active(True)
+	logout_switch.set_halign(1)
+	grid.attach(logout_switch, 1, 2, 1, 1)
+
+	if self.get_prime_status() == "intel":
+	    self.set_sensitive_button(self.nvidia_button, self.intel_button)
+	else:
+	    self.set_sensitive_button(self.intel_button, self.nvidia_button)
+
+	self.intel_button.connect("clicked", self.on_intel_clicked)
+	self.nvidia_button.connect("clicked", self.on_nvidia_clicked)
+
+    def get_prime_status(self):
+	if "intel" in subprocess.check_output(["prime-select", "query"]):
+	    return "intel"
+	else:
+	    return "nvidia"
+
+    def set_sensitive_button(self, activate, deactivate):
+	activate.set_sensitive(True)
+	deactivate.set_sensitive(False)
+
+    def on_intel_clicked(self, button):
+	self.set_sensitive_button(self.nvidia_button, self.intel_button)
+
+    def on_nvidia_clicked(self, button):
+	self.set_sensitive_button(self.intel_button, self.nvidia_button)
+
+win = PrimeSelector()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
